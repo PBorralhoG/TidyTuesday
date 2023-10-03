0aa4b00bcdd2493fea8dc63c98e41dfe3dba8238
Pedro Borralho <pedro.borralho@baseform.com>
2023-10-03 12:49:45 +0100

    Initial data exploration

--- 2023/week-40-US-grants/US_grants.R
+++ 2023/week-40-US-grants/US_grants.R
@@ -1,1 +1,60 @@
 
+library(tidytuesdayR)
+library(stringr)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
+###############################################################################################################################################################################################
+
+# tuesdata <- tidytuesdayR::tt_load('2023-10-03')
+tuesdata <- tidytuesdayR::tt_load(2023, week = 40)
+
+grants <- tuesdata$grants
+grant_opportunity_details <- tuesdata$grant_opportunity_details
+
+rm(tuesdata)
+
+###############################################################################################################################################################################################
+# # grants: relevant atts
+# opportunity_id
+# opportunity_number + opportunity_title
+# agency_code + agency_name
+# estimated_funding
+
+# # grant_opportunity_details: relevant atts
+# opportunity_id
+# funding_opportunity_number + funding_opportunity_title
+# estimated_total_program_funding
+# agency_name
+# description
+# category_x + category_explanation
+
+###############################################################################################################################################################################################
+
+categ_cols <- colnames(grant_opportunity_details)[colnames(grant_opportunity_details) %>% substr(1, 8) == "category"]
+cols <- c("opportunity_id", categ_cols %>% setdiff("category_explanation"))
+grants2 <- grant_opportunity_details[, cols] %>% gather(category, bool, -1) %>% filter(bool) %>% select(-bool)
+# there are grant with more than one category
+
+table(grants2$category, useNA = "always") %>% sort(decreasing = T)
+
+summary(grant_opportunity_details$estimated_total_program_funding)
+plot(grant_opportunity_details$estimated_total_program_funding)
+
+###############################################################################################################################################################################################
+
+AI_grants <- grant_opportunity_details %>% filter(str_detect(description, "artificial intelligence"))
+plot(AI_grants$posted_date)
+plot(AI_grants$posted_date, AI_grants$estimated_total_program_funding)
+
+summary(AI_grants$estimated_total_program_funding)
+
+AI_grants2 <- AI_grants %>% mutate(posted_year = year(posted_date)) %>% group_by(posted_year) %>% summarise(estimated_total_program_funding = sum(estimated_total_program_funding, na.rm = T))
+plot(AI_grants2)
+
+
+AI_grants3 <- AI_grants %>% mutate(posted_year = year(posted_date)) %>% group_by(posted_year) %>% summarise(n_estimated_total_program_funding = n())
+plot(AI_grants3)
+
+
