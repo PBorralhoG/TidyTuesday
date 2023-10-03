9c3a7ad4c7f53b8236aeab64d224095f22b83959
PedroBorralho <pborralho.g@gmail.com>
2023-10-04 00:08:38 +0100

    some cleanup + climate and AI plot tests

--- 2023/week-40-US-grants/US_grants.R
+++ 2023/week-40-US-grants/US_grants.R
@@ -13,43 +13,6 @@ grants <- tuesdata$grants
 grant_opportunity_details <- tuesdata$grant_opportunity_details
 
 ###############################################################################################################################################################################################
-# # grants: relevant atts
-# opportunity_id
-# opportunity_number + opportunity_title
-# agency_code + agency_name
-# estimated_funding
-
-# # grant_opportunity_details: relevant atts
-# opportunity_id
-# funding_opportunity_number + funding_opportunity_title
-# estimated_total_program_funding
-# agency_name
-# description
-# category_x + category_explanation
-
-###############################################################################################################################################################################################
-
-# categ_cols <- colnames(grant_opportunity_details)[colnames(grant_opportunity_details) %>% substr(1, 8) == "category"]
-# cols <- c("opportunity_id", categ_cols %>% setdiff("category_explanation"))
-# categs_by_grant <- grant_opportunity_details[, cols] %>% gather(category, bool, -1) %>% filter(bool) %>% select(-bool)
-# # there are grants with more than one category
-# table(categs_by_grant$category, useNA = "always") %>% sort(decreasing = T)
-# 
-# df <- merge(categs_by_grant,
-#             grant_opportunity_details %>% select(opportunity_id, estimated_total_program_funding, posted_date),
-#             by = "opportunity_id", all.x = T) %>%
-#   filter(!is.na(estimated_total_program_funding) & estimated_total_program_funding > 0) %>%
-#   mutate(posted_year = year(posted_date))
-
-###############################################################################################################################################################################################
-
-grant_opportunity_details %>%
-  filter(!is.na(estimated_total_program_funding) & estimated_total_program_funding > 0) %>%
-  mutate(posted_year = year(posted_date)) %>%
-  group_by(posted_year) %>%
-  summarise(sum_funding = sum(estimated_total_program_funding)) %>%
-  ggplot(aes(x = posted_year, y = sum_funding)) +
-  geom_col()
 
 grants %>%
   filter(!is.na(estimated_funding) & estimated_funding > 0) %>%
@@ -59,19 +22,12 @@ grants %>%
   ggplot(aes(x = posted_year, y = sum_funding)) +
   geom_col()
 
-grant_opportunity_details %>%
-  filter(str_detect(description, "artificial intelligence|machine learning|data science|neural networks")) %>%
-  filter(!is.na(estimated_total_program_funding) & estimated_total_program_funding > 0) %>%
-  mutate(posted_year = year(posted_date)) %>%
-  group_by(posted_year) %>%
-  summarise(sum_funding = sum(estimated_total_program_funding)) %>%
-  ggplot(aes(x = posted_year, y = sum_funding)) +
-  geom_col()
-
 ###############################################################################################################################################################################################
 
 grants_AI <- grants %>% left_join(grant_opportunity_details %>% select(opportunity_id, description), by = "opportunity_id") %>%
-  filter(str_detect(description %>% tolower, "artificial intelligence|machine learning|data science")) %>%
+  # filter(str_detect(description %>% tolower, "artificial intelligence|machine learning|data science")) %>%
+  filter(str_detect(description %>% tolower, "artificial intelligence")) %>%
+  filter(!is.na(description)) %>%
   filter(!is.na(estimated_funding) & estimated_funding > 0) %>%
   mutate(posted_year = year(posted_date))
 
@@ -96,6 +52,47 @@ grants_climate %>%
 
 ###############################################################################################################################################################################################
 
+# AI_topics <- "artificial intelligence|machine learning|big data|data science|deep learning"
+AI_topics <- "artificial intelligence"
 
+df <- grants %>% left_join(grant_opportunity_details %>% select(opportunity_id, description), by = "opportunity_id") %>%
+  filter(!is.na(estimated_funding) & estimated_funding > 0) %>%
+  mutate(posted_year = year(posted_date),
+         climate = (str_detect(description %>% tolower, "climate") & !is.na(description)) | (str_detect(opportunity_title %>% tolower, "climate") & !is.na(opportunity_title)),
+         AI = (str_detect(description %>% tolower, AI_topics) & !is.na(description)) | (str_detect(opportunity_title %>% tolower, AI_topics) & !is.na(opportunity_title)))
+table(df$AI, useNA = "always")
 
+df %>%
+  group_by(posted_year, climate) %>%
+  summarise(sum_funding = sum(estimated_funding)) %>%
+  ggplot(aes(x = posted_year, y = sum_funding, fill = climate)) +
+  geom_col()
+
+df %>%
+  group_by(posted_year) %>%
+  summarise(sum_funding = sum(estimated_funding),
+            sum_funding_climate = sum(estimated_funding[climate])) %>%
+  ungroup() %>%
+  mutate(ratio_climate = sum_funding_climate/sum_funding)
+
+df %>%
+  filter(climate) %>%
+  group_by(posted_year) %>%
+  summarise(sum_funding = sum(estimated_funding)) %>%
+  ggplot(aes(x = posted_year, y = sum_funding)) +
+  geom_col()
+
+df %>%
+  filter(AI) %>%
+  group_by(posted_year) %>%
+  summarise(sum_funding = sum(estimated_funding)) %>%
+  ggplot(aes(x = posted_year, y = sum_funding)) +
+  geom_col()
+
+df %>%
+  filter(AI) %>%
+  group_by(posted_year) %>%
+  summarise(sum_funding = sum(estimated_funding)) %>%
+  ggplot(aes(x = posted_year, y = sum_funding)) +
+  geom_line()
 
