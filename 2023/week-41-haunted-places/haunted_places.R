b389487594a4840aa2044ca7db280ef8ad028e97
Pedro Borralho <pedro.borralho@baseform.com>
2023-10-10 18:51:02 +0100

    first plots to visualize haunted schools

--- 2023/week-41-haunted-places/haunted_places.R
+++ 2023/week-41-haunted-places/haunted_places.R
@@ -2,7 +2,9 @@
 library(tidytuesdayR)
 library(dplyr)
 library(stringr)
+library(sf)
 library(ggplot2)
+library(mapdata)
 
 ###############################################################################################################################################################################################
 
@@ -11,20 +13,35 @@ haunted_places <- tuesdata$haunted_places
 
 ###############################################################################################################################################################################################
 
-haunted_places %>% group_by(state) %>% summarise(n = n()) %>% arrange(desc(n))
+haunted_schools <- haunted_places %>%
+  filter(str_detect(location %>% tolower, "school|college|university|education|elementary")) %>%
+  mutate(longitude = ifelse(is.na(longitude), city_longitude, longitude),
+         latitude = ifelse(is.na(latitude), city_latitude, latitude)) %>%
+  filter(!is.na(longitude) & !is.na(latitude)) %>%
+  filter(!state %in% c("Alaska", "Hawaii")) %>%
+  st_as_sf(coords = c("longitude", "latitude"), crs = 4326, remove = F)
 
-########################################################
+plot(haunted_schools$geometry)
 
-locations_2check <- "school|college|university|education|courthouse|road|restaurant|blvd|beach|park|cemetery|lake|hill|hospital|hotel|tavern|pub|theater|bridge|motel|asylum|house|train|library|church"
+###############################################
 
-haunted_places %>% filter(!str_detect(location %>% tolower, locations_2check)) %>% pull(location) %>% unique
+haunted_schools_count <- haunted_schools %>% as.data.frame %>% group_by(state) %>% summarise(n = n()) %>%
+  mutate(state = tolower(state),
+         state = ifelse(state == "washington dc", "district of columbia", state))
 
-haunted_places$location %>% tolower %>% str_detect(locations_2check) %>% table
+us_states_map <- map_data("state") %>%
+  mutate(region = tolower(region)) %>%
+  left_join(haunted_schools_count, by = join_by(region == state))
 
-########################################################
+# setdiff(haunted_schools_count$state, us_states_map$region)
+# setdiff(us_states_map$region, haunted_schools_count$state)
 
-haunted_schools <- haunted_places %>% filter(str_detect(location %>% tolower, "school|college|university|education|elementary"))
+###############################################
 
-###############################################################################################################################################################################################
-
-
+ggplot(data = us_states_map, aes(x = long, y = lat, fill = n, group = group)) + 
+  geom_polygon(color = "white") +
+  scale_fill_gradient(low = "orange", high = "purple") +
+  # scale_fill_gradient2(midpoint = max(haunted_schools_count$n)/2, low = "orange", mid = "purple", high = "black") +
+  # guides(fill = "none") +
+  theme_void() +
+  coord_map(clip = "off")
