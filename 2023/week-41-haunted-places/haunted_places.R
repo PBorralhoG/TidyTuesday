569e73fbd470dc7e4160e95a303b60ed99557bcb
Pedro Borralho <pedro.borralho@baseform.com>
2023-10-10 17:39:50 +0100

    data exploration

--- 2023/week-41-haunted-places/haunted_places.R
+++ 2023/week-41-haunted-places/haunted_places.R
@@ -1,1 +1,30 @@
 
+library(tidytuesdayR)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+
+###############################################################################################################################################################################################
+
+tuesdata <- tidytuesdayR::tt_load(2023, week = 41)
+haunted_places <- tuesdata$haunted_places
+
+###############################################################################################################################################################################################
+
+haunted_places %>% group_by(state) %>% summarise(n = n()) %>% arrange(desc(n))
+
+########################################################
+
+locations_2check <- "school|college|university|education|courthouse|road|restaurant|blvd|beach|park|cemetery|lake|hill|hospital|hotel|tavern|pub|theater|bridge|motel|asylum|house|train|library|church"
+
+haunted_places %>% filter(!str_detect(location %>% tolower, locations_2check)) %>% pull(location) %>% unique
+
+haunted_places$location %>% tolower %>% str_detect(locations_2check) %>% table
+
+########################################################
+
+haunted_schools <- haunted_places %>% filter(str_detect(location %>% tolower, "school|college|university|education|elementary"))
+
+###############################################################################################################################################################################################
+
+
