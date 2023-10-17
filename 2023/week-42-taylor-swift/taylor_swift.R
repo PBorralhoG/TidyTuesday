7f797cdcebdef4cf6777a831eee3018df00512ca
Pedro Borralho <pedro.borralho@baseform.com>
2023-10-17 19:25:56 +0100

    cleanup + check song title in lyrics

--- 2023/week-42-taylor-swift/taylor_swift.R
+++ 2023/week-42-taylor-swift/taylor_swift.R
@@ -1,45 +1,34 @@
 
-# library(tidytuesdayR)
+library(taylor)
 library(dplyr)
+library(stringr)
 
 ################################################################################################
 # load data
-
-# tuesdata <- tidytuesdayR::tt_load(2023, week = 42)
-# 
-# taylor_album_songs <- tuesdata$taylor_album_songs
-# taylor_all_songs <- tuesdata$taylor_all_songs
-# taylor_albums <- tuesdata$taylor_albums
-
-# taylor_album_songs <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-10-17/taylor_album_songs.csv')
-# taylor_all_songs <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-10-17/taylor_all_songs.csv')
-# taylor_albums <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-10-17/taylor_albums.csv')
-
-library(taylor)
+ 
+# taylor_album_songs
+# taylor_all_songs
+# taylor_albums
 
 ################################################################################################
 
-plot(taylor_album_songs$album_release, taylor_album_songs$tempo)
+taylor_album_songs$lyrics <- lapply(1:nrow(taylor_album_songs), function(i) taylor_album_songs$lyrics[[i]]$lyric)
+taylor_album_songs$found_n <- sapply(1:nrow(taylor_album_songs),
+                                     function(i) str_count(taylor_album_songs$lyrics[i] %>% unlist %>% tolower, taylor_album_songs$track_name[i] %>% tolower) %>% sum)
 
-tempo_by_album <- taylor_album_songs %>%
+boxplot(taylor_album_songs$found_n ~ taylor_album_songs$album_name)
+
+sum(taylor_album_songs$found_n > 0) / nrow(taylor_album_songs) # 56%
+
+perc_by_album <- taylor_album_songs %>%
   group_by(album_name) %>%
-  summarise(tempo_median = median(tempo, na.rm = T),
-            album_release = unique(album_release))
+  summarise(perc = sum(found_n > 0) / n(),
+            album_release = unique(album_release)) %>%
+  arrange(desc(perc))
 
-plot(tempo_by_album$album_release, tempo_by_album$tempo_median)
+plot(perc_by_album$album_release, perc_by_album$perc)
 
-setdiff(taylor_albums$album_name, tempo_by_album$album_name)
-setdiff(tempo_by_album$album_name, taylor_albums$album_name)
 
-taylor_albums <- taylor_albums %>% left_join(tempo_by_album %>% select(album_name, tempo_median), by = "album_name")
 
-plot(taylor_albums$tempo_median, taylor_albums$metacritic_score)
-plot(taylor_albums$tempo_median, taylor_albums$user_score)
-plot(taylor_albums$metacritic_score, taylor_albums$user_score)
-
-################################################################################################
-
-taylor_album_songs$track_name[1]
-"blue eyes" %in% taylor_album_songs$lyrics[[1]]$lyric
 
 
