c69bdadd9232edd960f88f5c47d7c9113fe02f19
PedroBorralho <pborralho.g@gmail.com>
2023-10-21 14:19:36 +0100

    refine track filters

--- 2023/week-42-taylor-swift/taylor_swift.R
+++ 2023/week-42-taylor-swift/taylor_swift.R
@@ -13,28 +13,25 @@ library(ggplot2)
 
 ################################################################################################
 
-taylor_all_songs <- taylor_all_songs %>% filter(!is.na(album_name))
+# taylor_all_songs <- taylor_all_songs %>% select(album_name, ep, album_release, track_number, track_name, lyrics)
+
+taylor_all_songs <- taylor_all_songs %>%
+  filter(!is.na(album_name)) %>%
+  filter(!ep) %>%
+  filter(!album_name %in% c("Fearless (Taylor's Version)", "Red (Taylor's Version)")) %>%
+  mutate(track_name_clean = track_name %>% tolower %>%
+           str_replace("\\(piano version\\)", "") %>%
+           str_replace("\\(pop version\\)", "") %>%
+           str_replace("\\(acoustic version\\)", "") %>%
+           str_replace("\\(strings remix\\)", "") %>%
+           str_replace("\\(piano remix\\)", "") %>%
+           str_replace("\\(original demo recording\\)", "") %>%
+           str_replace("&", "and") %>%
+           str_replace("1", "one") %>%
+           str_squish(),
+         track_name_clean_v2 = track_name_clean %>% str_replace_all("[[:punct:]]", " ") %>% str_squish)
 
 taylor_all_songs$lyrics <- lapply(1:nrow(taylor_all_songs), function(i) taylor_all_songs$lyrics[[i]]$lyric)
-
-taylor_all_songs <- taylor_all_songs %>% mutate(track_name_clean = track_name %>% tolower %>%
-                                                  str_replace("\\(taylor's version\\)", "") %>% str_replace("\\[taylor's version\\]", "") %>%
-                                                  str_replace("\\(from the vault\\)", "") %>% str_replace("\\[from the vault\\]", "") %>%
-                                                  str_replace("\\(piano version\\)", "") %>%
-                                                  str_replace("\\(pop version\\)", "") %>%
-                                                  str_replace("\\(acoustic version\\)", "") %>%
-                                                  str_replace("\\(10 minute version\\)", "") %>%
-                                                  str_replace("\\(remix\\)", "") %>%
-                                                  str_replace("\\(strings remix\\)", "") %>%
-                                                  str_replace("\\(piano remix\\)", "") %>%
-                                                  str_replace("\\(more lana del rey\\)", "") %>%
-                                                  str_replace("\\(original demo recording\\)", "") %>%
-                                                  str_replace("\\(radio edit\\)", "") %>%
-                                                  str_replace("\\(alternate version\\)", "") %>%
-                                                  str_replace("&", "and") %>%
-                                                  str_squish(),
-                                                track_name_clean_v2 = track_name_clean %>% str_replace_all("[[:punct:]]", " ") %>% str_squish)
-
 taylor_all_songs$lyrics_clean <- sapply(1:nrow(taylor_all_songs), function(i) taylor_all_songs$lyrics[i] %>% unlist %>% tolower %>% paste0(collapse = " ") %>% str_squish)
 taylor_all_songs <- taylor_all_songs %>% mutate(lyrics_clean_v2 = lyrics_clean %>% str_replace_all("[[:punct:]]", " ") %>% str_squish)
 
@@ -58,9 +55,7 @@ taylor_all_songs2 <- rbind(songs_found_n_g0, songs_found_n_eq0) %>% arrange(albu
          xmax = ifelse(found_n > 0, xmin + 22, xmin + 11.5),
          album_name_num = factor(album_name) %>% as.numeric,
          ymax = album_name_num + 138,
-         ymin = ifelse(found_n > 0, album_name_num, album_name_num + 53)#,
-         # track_name_label = ifelse(found_n == 0, track_name, NA)
-         )
+         ymin = ifelse(found_n > 0, album_name_num, album_name_num + 53))
 
 aux_labels <- taylor_all_songs2 %>% filter(found_n > 0) %>% group_by(album_name) %>% filter(track_number == min(track_number))
 album_levels <- taylor_all_songs %>% arrange(album_release) %>% pull(album_name) %>% unique
@@ -69,13 +64,12 @@ ggplot(mapping = aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = 
   geom_rect(data = taylor_all_songs2 %>% filter(found_n > 0), color = "black") +
   geom_rect(data = taylor_all_songs2 %>% filter(found_n == 0), color = "black") +
   scale_fill_manual(values = c("TRUE" = "white", "FALSE" = "black")) +
-  # facet_grid(rows = vars(factor(album_name, levels = album_levels))) +
+  scale_y_continuous(limits = c(0, 200)) +
   facet_wrap( ~ factor(album_name, levels = album_levels), ncol = 2, dir = "v") +
-  geom_text(data = aux_labels, aes(x = xmin, y = ymax + 15, label = album_name), check_overlap = T, hjust = 0) +
-  # ggrepel::geom_text_repel(data = taylor_all_songs2, aes(x = xmax, y = ymax, label = track_name_label)) +
+  geom_text(data = aux_labels, aes(x = xmin, y = ymax + 30, label = album_name), check_overlap = T, hjust = 0) +
   coord_fixed(ratio = 1) +
   labs(title = "Are we singing the song title?",
-       subtitle = "Yes, yes we are",
+       subtitle = "Yes, yes we are (at least Taylor Swift is)",
        caption = paste0("#TidyTuesday week 42 2023 | Data: {taylor} R package | Plot: Pedro Borralho")) +
   theme_void() +
   theme(strip.text = element_blank(),
@@ -84,9 +78,12 @@ ggplot(mapping = aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = 
         plot.caption = element_text(size = 8, hjust = 0.5, margin = margin(t = 40)),
         # legend.position = "none",
         plot.margin = unit(c(1, 0.5, 1, 0.5), "cm"),
-        plot.background = element_rect(fill = alpha("#AA9EB6", 0.7), color = NA))
+        plot.background = element_rect(fill = "#AA9EB6", color = NA))
 
 # album_palettes
+
+# save plot
+ggsave(paste0("taylor_swift_", format(Sys.time(), "%Y%m%d"), ".png"), dpi = 320, width = 6, height = 6)
 
 
 
