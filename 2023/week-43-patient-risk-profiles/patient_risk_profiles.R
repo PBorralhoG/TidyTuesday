3ac5bd8265dddf0227aef87cb6ea5e439cf417ae
Pedro Borralho <pedro.borralho@baseform.com>
2023-10-23 11:22:34 +0100

    load data

--- 2023/week-43-patient-risk-profiles/patient_risk_profiles.R
+++ 2023/week-43-patient-risk-profiles/patient_risk_profiles.R
@@ -1,1 +1,16 @@
 
+library(tidytuesdayR)
+# library(dplyr)
+# library(stringr)
+# library(ggplot2)
+# library(showtext)
+
+################################################################################################
+# load data
+
+tuesdata <- tidytuesdayR::tt_load(2023, week = 43)
+patient_risk_profiles <- tuesdata$patient_risk_profiles
+
+################################################################################################
+# prepare data
+
