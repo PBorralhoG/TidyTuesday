65b1d558078167a3c42baa5e53bed8e262eca112
Pedro Borralho <pedro.borralho@baseform.com>
2023-10-23 11:39:56 +0100

    clean age group columns

--- 2023/week-43-patient-risk-profiles/patient_risk_profiles.R
+++ 2023/week-43-patient-risk-profiles/patient_risk_profiles.R
@@ -1,7 +1,8 @@
 
 library(tidytuesdayR)
-# library(dplyr)
-# library(stringr)
+library(dplyr)
+library(stringr)
+library(tidyr)
 # library(ggplot2)
 # library(showtext)
 
@@ -14,3 +15,13 @@ patient_risk_profiles <- tuesdata$patient_risk_profiles
 ################################################################################################
 # prepare data
 
+age_cols <- colnames(patient_risk_profiles)[colnames(patient_risk_profiles) %>% str_detect("age group")]
+
+patient_risk_profiles <- patient_risk_profiles %>%
+  pivot_longer(age_cols, names_to = "age_group", values_to = "age_bool") %>%
+  filter(age_bool == 1) %>% select(-age_bool)
+
+patient_risk_profiles <- patient_risk_profiles %>%
+  mutate(age_group = age_group %>% str_split(":") %>% sapply("[[", 2) %>% str_squish,
+         age_group_min = age_group %>% str_split("-") %>% sapply("[[", 1) %>% as.numeric,
+         age_group_max = age_group %>% str_split("-") %>% sapply("[[", 2) %>% as.numeric)
