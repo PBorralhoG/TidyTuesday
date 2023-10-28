486d2af1d93b63897601a2855d6695d8e94ab7cd
PedroBorralho <pborralho.g@gmail.com>
2023-10-28 10:07:35 +0100

    commit to plot quantiles geom_ribbon and clean the script

--- 2023/week-43-patient-risk-profiles/patient_risk_profiles.R
+++ 2023/week-43-patient-risk-profiles/patient_risk_profiles.R
@@ -27,82 +27,29 @@ patient_risk_profiles <- patient_risk_profiles %>%
 risk_by_age_group <- patient_risk_profiles %>%
   group_by(age_group, disease) %>%
   summarise(pred_risk_min = min(pred_risk),
-            pred_risk_min2 = pred_risk %>% sort %>% nth(2),
             pred_risk_p5 = quantile(pred_risk, 0.05),
+            pred_risk_p25 = quantile(pred_risk, 0.25),
             pred_risk_med = quantile(pred_risk, 0.50),
+            pred_risk_p75 = quantile(pred_risk, 0.75),
             pred_risk_p95 = quantile(pred_risk, 0.95),
-            pred_risk_max2 = pred_risk %>% sort(decreasing = T) %>% nth(2),
             pred_risk_max = max(pred_risk),
             age_group_min = unique(age_group_min),
             age_group_mean = unique(age_group_mean),
             age_group_max = unique(age_group_max))
 
-risk_by_age_group <- risk_by_age_group %>%
-  group_by(disease) %>%
-  mutate(pred_risk_med_max = max(pred_risk_med),
-         pred_risk_med_norm = pred_risk_med / pred_risk_med_max,
-         pred_risk_med_med = median(pred_risk_med),
-         pred_risk_med_norm2 = pred_risk_med / pred_risk_med_med)
-
 ################################################################################################
-
-ggplot(patient_risk_profiles %>% filter(disease == "Dementia"), aes(x = age_group_mean, y = pred_risk)) +
-  geom_point()
-
-ggplot() +
-  # geom_rect(data = risk_by_age_group, aes(xmin = age_group_min, xmax = age_group_max+1, ymin = pred_risk_min, ymax = pred_risk_max), color = "black") +
-  geom_rect(data = risk_by_age_group, aes(xmin = age_group_min, xmax = age_group_max+1, ymin = pred_risk_p5, ymax = pred_risk_p95), fill = alpha("purple", 0.5), color = "black") +
-  # geom_rect(data = risk_by_age_group, aes(xmin = age_group_min, xmax = age_group_max+1, ymin = pred_risk_min2, ymax = pred_risk_max2), fill = alpha("red", 0.5), color = "black") +
-  geom_point(data = patient_risk_profiles, aes(x = age_group_mean+0.5, y = pred_risk)) +
-  # geom_point(data = risk_by_age_group, aes(x = age_group_mean+0.5, y = pred_risk_med), shape = 4, size = 3, stroke = 2) +
-  facet_wrap(~disease, scales = "free")
-
-#############################
-
-ggplot(risk_by_age_group, aes(x = age_group_mean, y = pred_risk_med, group = disease)) +
-  # geom_line() +
-  geom_smooth(se = FALSE, linewidth = 1)
-
-ggplot(risk_by_age_group, aes(x = age_group_mean, y = pred_risk_med, colour = disease)) +
-  # geom_line() +
-  geom_smooth(se = FALSE, linewidth = 1) +
-  gghighlight::gghighlight(use_direct_label = F, unhighlighted_params = list(colour = "grey")) +
-  facet_wrap(~disease) +
-  theme(legend.position = "none")
-
-ggplot(risk_by_age_group, aes(x = age_group_mean, y = pred_risk_med_norm, colour = disease)) +
-  # geom_line(lwd = 1) +
-  geom_smooth(se = FALSE, linewidth = 1) +
-  gghighlight::gghighlight(use_direct_label = F, unhighlighted_params = list(colour = grey(0.85))) +
-  facet_wrap(~disease) +
-  theme(legend.position = "none")
-
-################################################################################################
-
-table(FEMALE = patient_risk_profiles0$`Sex = FEMALE`, MALE = patient_risk_profiles0$`Sex = MALE`)
-
-patient_risk_profiles %>% select(personId, age_group) %>% unique %>% pull(age_group) %>% table
-xx <- patient_risk_profiles %>% select(personId, age_group_mean) %>% unique %>% pull(age_group_mean) %>% table
-plot(xx)
-
-################################################################################################
-
-risk_by_age_group %>% filter(disease %in% c("Dementia", "Migraine")) %>%
-  ggplot() +
-  geom_ribbon(aes(x = age_group_mean, ymin = pred_risk_min, ymax = pred_risk_max), fill = grey(0.8)) +
-  geom_ribbon(aes(x = age_group_mean, ymin = pred_risk_p5, ymax = pred_risk_p95), fill = grey(0.6)) +
-  geom_line(aes(x = age_group_mean, y = pred_risk_med), color = grey(0.2)) +
-  facet_wrap(~disease, scales = "free", nrow = 2)
+# plot
 
 risk_by_age_group2 <- risk_by_age_group %>% filter(disease %in% c("Dementia", "Migraine"))
 risk_by_age_group2 <- rbind(
-  risk_by_age_group2 %>% select(disease, age_group = age_group_min, pred_risk_min, pred_risk_p5, pred_risk_med, pred_risk_p95, pred_risk_max),
-  risk_by_age_group2 %>% select(disease, age_group = age_group_max, pred_risk_min, pred_risk_p5, pred_risk_med, pred_risk_p95, pred_risk_max))
+  risk_by_age_group2 %>% select(disease, age_group = age_group_min, pred_risk_min, pred_risk_p5, pred_risk_p25, pred_risk_med, pred_risk_p75, pred_risk_p95, pred_risk_max),
+  risk_by_age_group2 %>% select(disease, age_group = age_group_max, pred_risk_min, pred_risk_p5, pred_risk_p25, pred_risk_med, pred_risk_p75, pred_risk_p95, pred_risk_max))
 
 risk_by_age_group2 %>%
   ggplot() +
   geom_ribbon(aes(x = age_group, ymin = pred_risk_min, ymax = pred_risk_max), fill = grey(0.8)) +
   geom_ribbon(aes(x = age_group, ymin = pred_risk_p5, ymax = pred_risk_p95), fill = grey(0.6)) +
+  geom_ribbon(aes(x = age_group, ymin = pred_risk_p25, ymax = pred_risk_p75), fill = grey(0.4)) +
   geom_line(aes(x = age_group, y = pred_risk_med), color = grey(0.2)) +
   facet_wrap(~disease, scales = "free", nrow = 2)
   
