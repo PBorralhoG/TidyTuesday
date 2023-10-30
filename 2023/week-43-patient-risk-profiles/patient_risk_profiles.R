d56a5d4145c438c14cc60dc784a9830a67e2b2bd
Pedro Borralho <pedro.borralho@baseform.com>
2023-10-30 11:58:57 +0000

    adjusted text color and size

--- 2023/week-43-patient-risk-profiles/patient_risk_profiles.R
+++ 2023/week-43-patient-risk-profiles/patient_risk_profiles.R
@@ -58,9 +58,10 @@ axis_text <- rbind(data.frame(x = -3, y = 0, label = "0%", disease = "Dementia",
                    data.frame(x = -3, y = 0.07, label = "7%", disease = "Migraine", angle = 0),
                    data.frame(x = 0, y = 0.07*1.1, label = "0", disease = "Migraine", angle = 0),
                    data.frame(x = 94/2, y = 0.07*1.1, label = "Age", disease = "Migraine", angle = 0),
-                   data.frame(x = 94.5, y = 0.07*1.1, label = "94", disease = "Migraine", angle = 0),
-                   data.frame(x = 94*0.5, y = 0.4*0.6, label = "Dementia", disease = "Dementia", angle = 0),
-                   data.frame(x = 94*0.5, y = 0.07*0.6, label = "Migraine", disease = "Migraine", angle = 0))
+                   data.frame(x = 94.5, y = 0.07*1.1, label = "94", disease = "Migraine", angle = 0))
+
+facet_text <- rbind(data.frame(x = 94*0.5, y = 0.4*0.6, label = "**Dementia**", disease = "Dementia", angle = 0),
+                    data.frame(x = 94*0.5, y = 0.07*0.6, label = "**Migraine**", disease = "Migraine", angle = 0))
 
 aux <- risk_by_age_group2 %>% filter(age_group == max(age_group) & disease == "Dementia")
 
@@ -71,8 +72,10 @@ leg_lines <- rbind(data.frame(x = 99, xend = 99, y = aux$pred_risk_p25, yend = a
                    data.frame(x = 95.5, xend = 96, y = aux$pred_risk_p5, yend = aux$pred_risk_p5, disease = "Dementia"),
                    data.frame(x = 95.5, xend = 96, y = aux$pred_risk_p95, yend = aux$pred_risk_p95, disease = "Dementia"))
 
-leg_text <- rbind(data.frame(x = 97, y = aux$pred_risk_med, label = "90% of the patients", angle = -90, disease = "Dementia"),
-                  data.frame(x = 100, y = aux$pred_risk_med, label = "50% of the patients", angle = -90, disease = "Dementia"))
+leg_text <- rbind(data.frame(x = 97, y = aux$pred_risk_med, label = "90% of patients", angle = -90, disease = "Dementia"),
+                  data.frame(x = 100, y = aux$pred_risk_med, label = "50% of patients", angle = -90, disease = "Dementia"))
+
+axis_color <- "#6F4E37"
 
 ggplot(risk_by_age_group2) +
   geom_ribbon(aes(x = age_group, ymin = pred_risk_min, ymax = pred_risk_max), fill = grey(0.9)) +
@@ -81,19 +84,24 @@ ggplot(risk_by_age_group2) +
   geom_line(aes(x = age_group, y = pred_risk_med), color = grey(0.2)) +
   facet_wrap(~disease, scales = "free", nrow = 2) +
   scale_x_continuous(limits = c(-3, 100)) +
-  geom_segment(data = axis_lines, aes(x = x, y = y, xend = xend, yend = yend), linewidth = 0.5) +
-  geom_richtext(data = axis_text, aes(x = x, y = y, label = label, angle = angle), label.colour = NA) +
-  geom_segment(data = leg_lines, aes(x = x, y = y, xend = xend, yend = yend), linewidth = 0.5) +
-  # geom_richtext(data = leg_text, aes(x = x, y = y, label = label, angle = angle), label.colour = NA) +
-  geom_text(data = leg_text, aes(x = x, y = y, label = label, angle = angle), hjust = 0.4) + # 0.6
+  geom_segment(data = axis_lines, aes(x = x, y = y, xend = xend, yend = yend), linewidth = 0.5, color = axis_color) +
+  geom_richtext(data = axis_text, aes(x = x, y = y, label = label, angle = angle), fill = "white", label.colour = NA, size = 9/.pt, color = axis_color) +
+  # geom_text(data = facet_text, aes(x = x, y = y, label = label, angle = angle), hjust = 0.5, size = 12/.pt) +
+  geom_richtext(data = facet_text, aes(x = x, y = y, label = label, angle = angle), hjust = 0.5, size = 12/.pt, fill = NA, label.colour = NA, color = axis_color) +
+  geom_segment(data = leg_lines, aes(x = x, y = y, xend = xend, yend = yend), linewidth = 0.5, color = axis_color) +
+  geom_text(data = leg_text, aes(x = x, y = y, label = label, angle = angle), hjust = 0.4, size = 9/.pt, color = axis_color) +
   labs(title = "Patient Risk Profiles",
        subtitle = "Predicted risk of Dementia and Migraine by age of 100 simulated patients",
        caption = paste0("#TidyTuesday week 43 2023 | Data: R/Pharma | Plot: Pedro Borralho")) +
   theme_void() +
   theme(strip.text = element_blank(),
-        plot.title = element_text(size = 20, hjust = 0.5, face = "bold"),
-        plot.subtitle = element_text(size = 8.5, hjust = 0.5, margin = margin(t = 10, b = 40)),
-        plot.caption = element_text(size = 8, hjust = 0.5, margin = margin(t = 40)),
-        plot.margin = unit(c(1, 0.5, 1, 0.5), "cm"))
+        plot.title = element_text(size = 18, hjust = 0.5, face = "bold"),
+        plot.subtitle = element_text(size = 10, hjust = 0.5, margin = margin(t = 10, b = 40)),
+        plot.caption = element_text(size = 9, hjust = 0.5, margin = margin(t = 40)),
+        plot.margin = unit(c(1, 0.5, 1, 0.5), "cm"),
+        plot.background = element_rect(fill = "white", color = NA))
 
+# save plot
+ggsave(paste0("2023/week-43-patient-risk-profiles/patient_risk_", format(Sys.time(), "%Y%m%d"), ".png"),
+       dpi = 320, width = 8, height = 8)
 
